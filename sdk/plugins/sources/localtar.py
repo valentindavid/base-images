ecb81f46ab65028d137b37c4e59b44ddc57644e5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-20 11:02:15 +0000

    Adding tarfile member sanitization to extractall()

--- sdk/plugins/sources/localtar.py
+++ sdk/plugins/sources/localtar.py
@@ -36,7 +36,26 @@ class LocalTarSource(Source):
     def stage(self, directory):
         try:
             with tarfile.open(self.fullpath) as tar:
-                tar.extractall(path=directory)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=directory)
 
         except (tarfile.TarError, OSError) as e:
             raise SourceError("{}: Error staging source: {}".format(self, e)) from e
